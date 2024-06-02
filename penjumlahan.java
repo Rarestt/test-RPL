0ec7ff942e65dbd0f6e028505b785c582f623f33
akira <105222040@student.universitaspertamina.ac.id>
2024-06-02 23:29:41 +0700

    adding method penjumlahan

---
+++ penjumlahan.java
@@ -0,0 +1,6 @@
+public class penjumlahan {
+    static double hasil = 2.9875 + 0.9543;
+    public static void main(String[] args) {
+        System.out.println("hasil penjumlahan 2.9875 dengan 0.9543 adalah " + hasil);
+    }
+}
